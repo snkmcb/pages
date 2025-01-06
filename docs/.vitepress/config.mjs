bdd8133d40216a334cb7fb3bfc6f283d50addfc4
snkmcb <epidural666@gmail.com>
2025-01-06 16:28:35 +0900

    VitePress 初回導入

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  
+  // GitHubPages への展開
+  // https://qiita.com/watame/items/42d2966c4be3cb4b052c
+  base: "/_penguinnote/",
+  
+  title: "Penguin Note",
+  description: "A Portfolio Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
